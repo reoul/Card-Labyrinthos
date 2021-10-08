0d3ba6685660bfd00792f8126aed48b72e52d3de
ReoulRiul <dydqja1358@gmail.com>
2021-10-08 18:18:30 +0900

    FadeInOut 기능 수정
    FadeInOut 기능 수정

---
+++ GStarProject/Assets/Scripts/FadeManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager Inst { get; private set; }
+    SpriteRenderer SR;
+
+    public static event EventHandler FadeEvent;
+
+    private void Awake()
+    {
+        Inst = this;
+        DontDestroyOnLoad(this.gameObject);
+        SR = this.GetComponent<SpriteRenderer>();
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(Fade(false));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(true));
+    }
+
+    IEnumerator Fade(bool isOut)
+    {
+        float alpha = isOut ? 0 : 1;
+        if (isOut)
+            while (alpha < 1)
+            {
+                alpha += Time.deltaTime;
+                SR.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
+                yield return new WaitForEndOfFrame();
+            }
+        else
+            while (alpha > 0)
+            {
+                alpha -= Time.deltaTime;
+                Debug.Log(alpha);
+                SR.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
+                yield return new WaitForEndOfFrame();
+            }
+        FadeEvent.Invoke(this, EventArgs.Empty);
+    }
+
+    public void TestEvent(object sender, EventArgs e)
+    {
+
+    }
+}
--- GStarProject/Assets/Scripts/GameManager.cs
+++ GStarProject/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 
 public class GameManager : MonoBehaviour
@@ -15,34 +16,13 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         Inst = this;
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    //public GameObject aaaa;
-
-    // Start is called before the first frame update
     void Start()
     {
-        StartGame();
+        //StartGame();
         DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-#if UNITY_EDITOR
-        InputCheatKey();
-#endif
-
-        //if(TouchManager.inst.IsTouchLeft())
-        //    Debug.Log(Vector2.Distance(Utils.TouchPos,aaaa.transform.position));
-    }
-
-    void InputCheatKey()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-            TurnManager.OnAddCard.Invoke();
-        if (Input.GetKeyDown(KeyCode.W))
-            TurnManager.Inst.EndTurn();
     }
 
     public void StartGame()
@@ -52,35 +32,5 @@ public class GameManager : MonoBehaviour
     public void Notification(string message)
     {
         notificationPanel.Show(message);
-    }
-
-    public void FadeIn()
-    {
-        StartCoroutine(Fade(false));
-    }
-
-    public void FadeOut()
-    {
-        StartCoroutine(Fade(true));
-    }
-
-    IEnumerator Fade(bool isOut)
-    {
-        float alpha = isOut ? 0 : 1;
-        if (isOut)
-            while (alpha < 1)
-            {
-                alpha += Time.deltaTime;
-                fadeInOutObj.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
-                yield return new WaitForEndOfFrame();
-            }
-        else
-            while (alpha > 0)
-            {
-                alpha -= Time.deltaTime;
-                Debug.Log(alpha);
-                fadeInOutObj.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
-                yield return new WaitForEndOfFrame();
-            }
     }
 }
