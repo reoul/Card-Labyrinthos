0c01cbb5e1e1d53d9d65b04a6055a915a2e91010
KangHaNul <kj456379@gmail.com>
2021-10-08 22:46:39 +0900

    heal
    heal

---
+++ GStarProject/Assets/Scripts/UI/Heal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+   // public HpBar hpbar();
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("휴식방에 입장했습니다!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        Debug.Log("체력이 40회복 되었습니다!");
+      //  hpbar.heal(40);
+    }
+}
