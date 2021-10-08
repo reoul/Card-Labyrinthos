ea94fdedad97f467b8d615ae805963cd36c244b4
ReoulRiul <dydqja1358@gmail.com>
2021-10-08 18:20:40 +0900

    맵 파일 변경
    맵 파일 변경

---
+++ GStarProject/Assets/Scripts/MapIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapIcon : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        MapManager.Inst.IconMouseDown();
+    }
+
+    private void OnMouseUp()
+    {
+        MapManager.Inst.IconMouseUp();
+    }
+}
---
+++ GStarProject/Assets/Scripts/MapManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public static MapManager Inst { get; private set; }
+
+    private void Awake()
+    {
+        Inst = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void IconMouseDown()
+    {
+        Debug.Log("donw");
+    }
+    public void IconMouseUp()
+    {
+        FadeManager.Inst.FadeOut();
+    }
+}
