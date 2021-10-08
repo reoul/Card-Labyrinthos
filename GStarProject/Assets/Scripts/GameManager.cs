527cd918bf201b8b3e7db53b6a43df1ed43175e4
ReoulRiul <dydqja1358@gmail.com>
2021-10-08 17:11:54 +0900

    FadeInOut 시스템 구현
    FadeInOut 시스템 구현

--- GStarProject/Assets/Scripts/GameManager.cs
+++ GStarProject/Assets/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] Notification notificationPanel;
 
+    [SerializeField] SpriteRenderer fadeInOutObj;
+
     void Awake()
     {
         Inst = this;
@@ -50,5 +52,35 @@ public class GameManager : MonoBehaviour
     public void Notification(string message)
     {
         notificationPanel.Show(message);
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(Fade(false));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade(true));
+    }
+
+    IEnumerator Fade(bool isOut)
+    {
+        float alpha = isOut ? 0 : 1;
+        if (isOut)
+            while (alpha < 1)
+            {
+                alpha += Time.deltaTime;
+                fadeInOutObj.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
+                yield return new WaitForEndOfFrame();
+            }
+        else
+            while (alpha > 0)
+            {
+                alpha -= Time.deltaTime;
+                Debug.Log(alpha);
+                fadeInOutObj.color = new Color(0, 0, 0, Mathf.Clamp01(alpha));
+                yield return new WaitForEndOfFrame();
+            }
     }
 }
