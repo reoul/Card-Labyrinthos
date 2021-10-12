af4f8bcf0085c4dd9809edd1bacbcfbe32436360
reoul <dydqja1358@gmail.com>
2021-10-12 17:55:01 +0900

    상점 기능 추가
    상점 기능 추가

---
+++ Assets/Scripts/ShopItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SHOPITEM_TYPE { CARD, SKILLPAGE, ADD_DRAW }
+
+public class Item
+{
+    public SHOPITEM_TYPE type;
+    public int index;
+}
+
+public class ShopItem : MonoBehaviour
+{
+    public Item item;
+    bool onItem = false;    //마우스가 아이템 위에 있는지
+
+    private void OnMouseUp()
+    {
+        if (onItem)
+            Debug.Log("aadsads");
+        //ShopManager.Inst.Click(this.item);
+    }
+
+    private void OnMouseEnter()
+    {
+        onItem = true;
+    }
+
+    private void OnMouseExit()
+    {
+        onItem = false;
+    }
+}
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public static ShopManager Inst;
+
+    public List<ShopItem> items;
+
+    private void Awake()
+    {
+        Inst = this;
+    }
+
+    public void Init()
+    {
+
+    }
+
+    public void Click(Item item)
+    {
+
+    }
+}
