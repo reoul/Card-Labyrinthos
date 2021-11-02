2a187cd1ac40573d715e3c2b005ec6b588a2fa6d
ReoulRiul <dydqja1358@gmail.com>
2021-11-03 04:44:45 +0900

    수정
    수정

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -37,9 +37,17 @@ public class StageManager : MonoBehaviour
             SoundManager.Inst.Play(BACKGROUNDSOUND.BATTLE);
             for (int i = 0; i < monsterSO.monsters.Length; i++)
             {
+                if (MapManager.Inst.fieldData.monster_type == MONSTER_TYPE.BOSS)
+                    SoundManager.Inst.Play(BACKGROUNDSOUND.BOSS);
+                else
+                    SoundManager.Inst.Play(BACKGROUNDSOUND.BATTLE);
                 if (MapManager.Inst.fieldData.monster_type == monsterSO.monsters[i].type)
                 {
                     Enemy enemy = Instantiate(monsterSO.monsters[i].prefab, enemy_spawn.position, Quaternion.identity).GetComponent<Enemy>();
+                    if (MapManager.Inst.fieldData.monster_type == MONSTER_TYPE.BOSS)
+                    {
+                        enemy.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    }
                     Vector3 enemy_position = enemy.hpbar.transform.position;
                     float position_x = enemy_position.x - enemy_spawn.position.x;
                     float position_y = enemy_position.y - enemy_spawn.position.y;
